5906f5b9ccbf646c4bc4df9f52415c4342150042
hyeonjeongJ <161409523+hyeonjeongJ@users.noreply.github.com>
2024-04-18 16:50:33 +0900

    Time: 225 ms (90.96%), Space: 18.8 MB (83.24%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,8 +1,17 @@
 class Solution:
-    def numIslands(self, grid):
-        if not grid:
-            return 0
+    def dfs(self, grid, i, j):
+        if i <= -1 or j <= -1 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] != '1':
+            return
+        # 한번 방문한 곳은 다시 방문하지 않도록 0으로 바꾸기
         
+        grid[i][j] = '0'
+        self.dfs(grid, i+1, j)
+        self.dfs(grid, i-1, j)
+        self.dfs(grid, i, j+1)
+        self.dfs(grid, i, j-1)    
+    
+    def numIslands(self, grid):
+
         count = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
@@ -11,11 +20,4 @@ class Solution:
                     count += 1
         return count
 
-    def dfs(self, grid, i, j):
-        if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] != '1':
-            return
-        grid[i][j] = '#'
-        self.dfs(grid, i+1, j)
-        self.dfs(grid, i-1, j)
-        self.dfs(grid, i, j+1)
-        self.dfs(grid, i, j-1)
+   

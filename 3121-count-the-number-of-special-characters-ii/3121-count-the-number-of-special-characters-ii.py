6c966fb4eb9551cade72be56c26f7a2cfcc9918f
hyeonjeongJ <161409523+hyeonjeongJ@users.noreply.github.com>
2024-04-26 17:37:06 +0900

    Time: 868 ms (9.08%), Space: 18.7 MB (53.32%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.py
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.py
@@ -1,32 +1,15 @@
-'''
+
 class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
-        word = set(word)
+        word_set = set(word)
         dic = {}
         for i, w in enumerate(word):
-            if w.islower() and (w.upper() in word):
+            if w.isupper() and (w.lower() in word):
                 if w not in dic:
                     dic[w] = i
                     
         for i, w in enumerate(word):
-            if w.isupper() and (w.lower() in dic):
-                if i < dic[w.lower()]:                    
-                    dic.pop(w.lower())
-        return len(dic)
-'''
-class Solution:
-    def numberOfSpecialChars(self, word: str) -> int:
-        wordSet = set(word)
-        dic = {}
-        special = 0
-        for i, char in enumerate(word):
-            if char.isupper() and char.lower() in wordSet:
-                if char not in dic:
-                    dic[char] = i
-        
-        for i, char in enumerate(word):
-            if char.islower() and char.upper() in dic:
-                if i > dic[char.upper()]:
-                    dic.pop(char.upper())
-        
+            if w.islower() and (w.upper() in dic):
+                if i > dic[w.upper()]:                    
+                    dic.pop(w.upper())
         return len(dic)

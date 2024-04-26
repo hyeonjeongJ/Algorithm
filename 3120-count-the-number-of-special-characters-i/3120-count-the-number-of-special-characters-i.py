7b419faea7c3aaeb3d66a00f897085495277bf23
hyeonjeongJ <161409523+hyeonjeongJ@users.noreply.github.com>
2024-04-26 16:21:01 +0900

    Time: 29 ms (93.92%), Space: 16.5 MB (43.44%) - LeetHub

---
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numberOfSpecialChars(self, word: str) -> int:
+        word = set(word)
+        count = 0
+        for w in word:
+            if w.lower() == w:
+                if w.upper() in word:
+                    count += 1
+                    
+        return count
+        
